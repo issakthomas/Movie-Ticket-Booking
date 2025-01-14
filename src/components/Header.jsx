0d785dead87aba65a418cefed82966915f0123e0
Issak <issakthomas22@gmail.com>
2025-01-14 11:37:13 +0530

    V1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,31 +3,31 @@ import "./Header.css";
 import { useEffect, useState } from "react";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [admin, setAdmin] = useState(false);
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [admin, setAdmin] = useState(false);
 
-    useEffect(() => {
-        if (admin && !location.pathname.startsWith("/admin")) {
-            navigate("/admin/add");
-        } else if (!admin && location.pathname.startsWith("/admin")) {
-            navigate("/");
-        }
-    }, [admin, navigate, location.pathname]);
+	useEffect(() => {
+		if (admin && !location.pathname.startsWith("/admin")) {
+			navigate("/admin/add");
+		} else if (!admin && location.pathname.startsWith("/admin")) {
+			navigate("/");
+		}
+	}, [admin, navigate, location.pathname]);
 
-    return (
-        <header>
-            <div className="logoSection" onClick={() => setAdmin(false)}>
-                <i className="fa-solid fa-ticket"></i>
-                <span>Cinepass</span>
-            </div>
+	return (
+		<header>
+			<div className="logoSection" onClick={() => navigate("/")}>
+				<i className="fa-solid fa-ticket"></i>
+				<span>Cinepass</span>
+			</div>
 
-            <i
-                onClick={() => setAdmin(!admin)}
-                className="fa-solid fa-circle-user user"
-            ></i>
-        </header>
-    );
+			<div className="iconSection" onClick={() => setAdmin(!admin)}>
+				<span style={{ display: !admin && "none" }}>ADMIN</span>
+				<i className="fa-solid fa-circle-user user"></i>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
